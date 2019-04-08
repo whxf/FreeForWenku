de4d6de77cb64f1caa439b4f7a06fe7fcb6524f2
lixi <whxf_lixi.buaa.edu.cn>
2019-04-08 16:33:25 +0800

    添加错误处理，更改request方式，添加title获取、命名文件

--- FreeForWenku.py
+++ FreeForWenku.py
@@ -1,110 +1,66 @@
-import requests
-import re
-import argparse
-import sys
 import json
 import os
+import re
+
+import requests
+
+session = requests.session()
+info_path = 'file_info.json'
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("url", help="Target Url,你所需要文档的URL",type=str)
-parser.add_argument('type', help="Target Type,你所需要文档的的类型(DOC|PPT|TXT|PDF)",type=str)
-args = parser.parse_args()
+def parse_title(content):
+    return re.findall(r"title.*?\:.*?\'(.*?)\'\,", content)[0]
 
-url = args.url
-type = args.type
 
-#根据文件决定函数
-y = 0
+def get_url_data(url):
+    """
+    获取文档链接的html链接和文档的title
+    :param url: 文档的链接
+    :return:
+    """
+    url_response = session.get(url)
+    html = url_response.content.decode('gbk')
+    title = parse_title(html)
+    return title, html
+
+
 def DOC(url):
-    doc_id = re.findall('view/(.*).html', url)[0]
-    html = requests.get(url).text
-    lists=re.findall('(https.*?0.json.*?)\\\\x22}',html)
-    lenth = (len(lists)//2)
-    NewLists = lists[:lenth]
-    for i in range(len(NewLists)) :
-        NewLists[i] = NewLists[i].replace('\\','')
-        txts=requests.get(NewLists[i]).text
-        txtlists = re.findall('"c":"(.*?)".*?"y":(.*?),',txts)
-        for i in range(0,len(txtlists)):
-            global y
-            print(txtlists[i][0].encode('utf-8').decode('unicode_escape','ignore'))
-            if y != txtlists[i][1]:
-                y = txtlists[i][1]
-                n = '\n'
-            else:
-                n = ''
-            filename = doc_id + '.txt'
-            with open(filename,'a',encoding='utf-8') as f:
-                f.write(n+txtlists[i][0].encode('utf-8').decode('unicode_escape','ignore').replace('\\',''))
-        print("文档保存在"+filename)
+    """
+    获取百度文档中doc文件的内容，并存储在txt文件中
+    :param url: doc文档的链接
+    :return:
+    """
+    title, html = get_url_data(url)
+    content_url_list = re.findall('(https.*?0.json.*?)\\\\x22}', html)
+    url_list_len = (len(content_url_list) // 2)
+    content_url_list = content_url_list[:url_list_len]
+    filename = os.path.join('output', title + '.txt')
+    line_flag = 0  # 用来整合数据：相同时表示同一行数据，直接拼接；否则，需要在数据间加上换行符号
+    for content_url in content_url_list:
+        content_url = content_url.replace('\\', '')
+        try:
+            content_html = session.get(content_url).text
+            content_segments = re.findall('"c":"(.*?)".*?"y":(.*?),', content_html)
 
-
-
-
-def PPT(url):
-    doc_id = re.findall('view/(.*).html',url)[0]
-    url = "https://wenku.baidu.com/browse/getbcsurl?doc_id="+doc_id+"&pn=1&rn=99999&type=ppt"
-    html = requests.get(url).text
-    lists=re.findall('{"zoom":"(.*?)","page"',html)
-    for i in range(0,len(lists)):
-        lists[i] = lists[i].replace("\\",'')
-    try:
-        os.mkdir(doc_id)
-    except:
-        pass
-    for i in range(0,len(lists)):
-        img=requests.get(lists[i]).content
-        with open(doc_id+'\img'+str(i)+'.jpg','wb') as m:
-            m.write(img)
-    print("PPT图片保存在" + doc_id +"文件夹")
-
-def TXT(url):
-    doc_id = re.findall('view/(.*).html', url)[0]
-    url = "https://wenku.baidu.com/api/doc/getdocinfo?callback=cb&doc_id="+doc_id
-    html = requests.get(url).text
-    md5 = re.findall('"md5sum":"(.*?)"',html)[0]
-    pn = re.findall('"totalPageNum":"(.*?)"',html)[0]
-    rsign = re.findall('"rsign":"(.*?)"',html)[0]
-    NewUrl = 'https://wkretype.bdimg.com/retype/text/'+doc_id+'?rn='+pn+'&type=txt'+md5+'&rsign='+rsign
-    txt = requests.get(NewUrl).text
-    jsons = json.loads(txt)
-    texts=re.findall("'c': '(.*?)',",str(jsons))
-    print(texts)
-    filename=doc_id+'.txt'
-    with open(filename,'a',encoding='utf-8') as f:
-        for i in range(0,len(texts)):
-            texts[i] = texts[i].replace('\\r','\r')
-            texts[i] = texts[i].replace('\\n','\n')
-
-            f.write(texts[i])
-    print("文档保存在" + filename)
-
-def PDF(url):
-    doc_id = re.findall('view/(.*).html',url)[0]
-    url = "https://wenku.baidu.com/browse/getbcsurl?doc_id="+doc_id+"&pn=1&rn=99999&type=ppt"
-    html = requests.get(url).text
-    lists=re.findall('{"zoom":"(.*?)","page"',html)
-    for i in range(0,len(lists)):
-        lists[i] = lists[i].replace("\\",'')
-    try:
-        os.mkdir(doc_id)
-    except:
-        pass
-    for i in range(0,len(lists)):
-        img=requests.get(lists[i]).content
-        with open(doc_id+'\img'+str(i)+'.jpg','wb') as m:
-            m.write(img)
-    print("FPD图片保存在" + doc_id + "文件夹")
+            # segment[0] 表示文字内容 segment[1] 表示文字所在行的标注，与line_flag对应
+            for segment in content_segments:
+                segment_join = ''
+                if line_flag != segment[1]:
+                    line_flag = segment[1]
+                    segment_join = '\n'
+                with open(filename, 'a', encoding='utf-8') as f:
+                    f.write(
+                        segment_join + segment[0].encode('utf-8').decode('unicode_escape', 'ignore').replace('\\', ''))
+        except Exception:
+            print('爬取失败！出错位置：')
+            print('url: {}\ncontent segment url: {}'.format(url, content_url))
+    print('Finish process {}'.format(url))
 
 
 if __name__ == "__main__":
-    try:
-        print("""
-###Athor:Lz1y
-###Blog:www.Lz1y.cn
-###TIPS:PDF|PPT只能下载图片
-        """)
+    with open(info_path, 'r', encoding='utf-8') as f:
+        info_data = json.loads(f.read())
+    for item in info_data:
+        url = item['url']
+        type = item['type']
         eval(type.upper())(url)
-    except:
-        print("获取出错，可能URL错误\n使用格式name.exe url type\n请使用--help查看帮助")
--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # FreeForWenku
 
-免费下载百度文库收费资料
+免费下载百度文库收费资料，目前本项目仅对原项目的doc爬取优化，其余的待完成优化。
 
 本项目fork from： https://github.com/Lz1y/FreeForWenku
+
+
+## 使用方法：
+
+### Step 1： 定义需要爬去的文件链接信息
+
+文件链接信息存储在```file_info.json```文件中，存储格式为：
+```
+[
+  {
+    "type": "doc",
+    "url": "https://wenku.baidu.com/view/9ff1b7e64a7302768e9939f4.html?from=search"
+  },````
+  {
+    "type": "",
+    "url": ""
+  }
+]
+```
+
+### Step 2： 运行```FreeForWenku.py```
+
+本脚本支持DOC文档的爬取，爬去结果会以TXT文件的形式存储在```output```文件夹中，文件命名为```file_title.txt```
 
